a98540a08bfd80ac3bba2a9ad258dad54ee87c57
Alfredo Moya <osmarwine@pm.me>
2023-10-25 20:13:24 -0500

    Triste intento de guardar en el localStorage

--- main.js
+++ main.js
@@ -1,52 +1,18 @@
-const btnComics = document.querySelector('#btn-comics');
-btnComics.addEventListener("click", () => {
-    const categoria = btnComics.getAttribute("data-categoria");
-    tienda(categoria);
-});
-
-const btnFiguras = document.querySelector('#btn-figuras');
-btnFiguras.addEventListener("click", () => {
-    const categoria = btnFiguras.getAttribute("data-categoria");
-    tienda(categoria);
-});
-
-const btnRopa = document.querySelector('#btn-ropa');
-btnRopa.addEventListener("click", () => {
-    const categoria = btnRopa.getAttribute("data-categoria");
-    tienda(categoria);
-});
-
-const tienda = (categoria) => {
-    const opcion = prompt(`Bienvenido a nuestra tienda. Dinos qué te interesa comprar:\n1. Comprar Comics\n2. Comprar Figuras de Acción\n3. Comprar Ropa y Accesorios\n4. Salir`);
+const tienda = () => {
+    let opcion = parseInt(prompt('Bienvenido a nuestra tienda. Dinos qué te interesa comprar:\n1. Cómics\n2. Figuras de Acción\n3. Ropa y Accesorios\n4. Salir'));
 
     switch (opcion) {
-        case "1":
-            comprar(categoria);
-            break;
-        case "2":
-            comprar(categoria);
-            break;
-        case "3":
-            comprar(categoria);
-            break;
-        case "4":
-            alert('Gracias por visitar nuestra tienda.');
-            break;
-        default:
-            alert('Gracias por visitar nuestra tienda.');
-    }
-}
-
-const comprar = (categoria) => {
-    switch (categoria) {
-        case "Cómics":
+        case 1:
             comprarComicsMangasLibros();
             break;
-        case "Figuras de Acción":
+        case 2:
             comprarFigurasDeAccion();
             break;
-        case "Ropa y Accesorios":
+        case 3:
             comprarRopaYAccesorios();
+            break;
+        case 4:
+            alert('Gracias por visitar nuestra tienda.');
             break;
         default:
             alert('Gracias por visitar nuestra tienda.');
@@ -205,34 +171,3 @@ const comprarRopaYAccesorios = () => {
     }
 }
 tienda();
-
-
-btnComics.addEventListener("click", () => {
-    const listaComics = document.querySelector("#lista-comics");
-
-    if (listaComics.style.display === "none") {
-        listaComics.style.display = "block";
-    } else {
-        listaComics.style.display = "none";
-    }
-});
-
-btnFiguras.addEventListener("click", () => {
-    const listaFiguras = document.querySelector("#lista-figuras");
-
-    if (listaFiguras.style.display === "none") {
-        listaFiguras.style.display = "block";
-    } else {
-        listaFiguras.style.display = "none";
-    }
-});
-
-btnRopa.addEventListener("click", () => {
-    const listaRopa = document.querySelector("#lista-ropa");
-
-    if (listaRopa.style.display === "none") {
-        listaRopa.style.display = "block";
-    } else {
-        listaRopa.style.display = "none";
-    }
-});
